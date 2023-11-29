debc38ebfe9b9e96c18f4bb47312c634b1725b92
Ксенія Серховець <Kseniia.Serkhovets@infiz.khpi.edu.ua>
2023-11-29 07:04:31 +0200

    Lab 7.2

--- lab7/lab7.sql
+++ lab7/lab7.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `news_categoris` (
 CREATE TABLE IF NOT EXISTS `news_comments` (
   `id` int NOT NULL AUTO_INCREMENT,
   `news_id` int NOT NULL,
-  `user_ip` int NOT NULL,
+  `user_ip` int unsigned NOT NULL,
   `title` varchar(255) NOT NULL,
   `comment_text` text NOT NULL,
   `date` datetime NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `news_comments` (
 CREATE TABLE IF NOT EXISTS `news_ratings` (
   `id` int NOT NULL AUTO_INCREMENT,
   `news_id` int NOT NULL UNIQUE,
-  `user_ip` int NOT NULL UNIQUE,
+  `user_ip` int unsigned NOT NULL UNIQUE,
   `mark` int NOT NULL,
   PRIMARY KEY (`id`)
 );
