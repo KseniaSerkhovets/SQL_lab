d04ba617f1598b6d5c88a5ab5fbd7ad24bbe3eab
Ксенія Серховець <Kseniia.Serkhovets@infiz.khpi.edu.ua>
2023-11-29 06:58:46 +0200

    Lab 8

---
+++ lab8/lab8.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS `projects` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `links` text,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `project_id` int NOT NULL,
+  `author_id` int NOT NULL,
+  `links` text,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ip` int unsigned NOT NULL,
+  `username` varchar(255) NOT NULL,
+   PRIMARY KEY (`id`)
+) ;
+
+CREATE TABLE IF NOT EXISTS `task_participants` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `task_id` int NOT NULL UNIQUE,
+  `user_id` int NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `project_participants` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `project_id` int NOT NULL UNIQUE,
+  `user_id` int NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+);
